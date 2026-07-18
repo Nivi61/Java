1a9d6b02bf0faebedb8e7c2ee90e09bce9db1f29
Nivi61 <167299198+Nivi61@users.noreply.github.com>
2026-07-18 20:49:18 +0530

    Add files via upload

---
+++ day1/identifier.java
@@ -0,0 +1,14 @@
+public class identifier {
+        public static void main(String[]args){
+                System.out.println("hello");
+        }
+        
+}
+
+/*In above code there are 7 Identifiers: identifier (class name)
+                                         main (method name) This is the main method where Java starts executing your program.
+                                         String (class/type name)
+                                         args (parameter)
+                                         System (class name)
+                                         out (static feild of system)
+                                         println (method name) */
